dbe5c06f3c127a2fe9a81eb80e22ddefbf344eb9
WolfMAN <aria.azadi.pour@protonmail.com>
2020-05-06 23:26:28 +0430

    create user list component and add babel polyfill

---
+++ src/client/Components/UsersList.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+import {getUsers} from "../actions";
+
+class UsersList extends Component {
+    constructor(props) {
+        super(props);
+        this.renderUsers = this.renderUsers.bind(this);
+    }
+
+    componentDidMount() {
+        this.props.getUsers();
+    }
+
+    renderUsers() {
+        return this.props.users.map(user => <li key={user.name}>{user.name}</li>)
+    }
+
+    render() {
+        return (
+            <ul>
+                {this.renderUsers()}
+            </ul>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        users: state.users
+    };
+};
+
+export default connect(mapStateToProps, {getUsers})(UsersList);
