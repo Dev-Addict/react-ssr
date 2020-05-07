2388fc0de3bd338586e4d016a544c784bc4c0442
WolfMAN <aria.azadi.pour@protonmail.com>
2020-05-07 16:26:01 +0430

    create 404 page

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -3,20 +3,26 @@ import React from 'react';
 import App, {loadData as AppLoadData} from "./components/App";
 import HomePage from "./pages/HomePage";
 import UsersPage, {loadData as UsersPageLoadData} from "./pages/UsersPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 export default [
     {
         component: App,
         loadData: AppLoadData,
-        routes: [{
-            path: '/',
-            component: HomePage,
-            exact: true
-        },
+        routes: [
+            {
+                path: '/',
+                component: HomePage,
+                exact: true
+            },
             {
                 path: '/users',
                 component: UsersPage,
                 loadData: UsersPageLoadData
-            }]
+            },
+            {
+                component: NotFoundPage
+            }
+        ]
     }
 ];
