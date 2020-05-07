e2a2a6c7867b0934cf448308d881f40e90ab90cf
WolfMAN <aria.azadi.pour@protonmail.com>
2020-05-07 12:40:52 +0430

    create App and Header

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -1,17 +1,21 @@
 import React from 'react';
 
+import App from "./components/App";
 import HomePage from "./pages/HomePage";
-import UsersPage , {loadData as UsersPageLoadData} from "./pages/UsersPage";
+import UsersPage, {loadData as UsersPageLoadData} from "./pages/UsersPage";
 
 export default [
     {
-        path: '/',
-        component: HomePage,
-        exact: true
-    },
-    {
-        path: '/users',
-        component: UsersPage,
-        loadData: UsersPageLoadData
+        component: App,
+        routes: [{
+            path: '/',
+            component: HomePage,
+            exact: true
+        },
+            {
+                path: '/users',
+                component: UsersPage,
+                loadData: UsersPageLoadData
+            }]
     }
 ];
